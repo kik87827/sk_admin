a5b4cc6796fcfe8641c83ce5841f53fd3f8beff0
kiminkyung <kik87827@gmail.com>
2022-09-14 21:49:05 +0900

    test

---
+++ js/custom.js
@@ -0,0 +1,30 @@
+document.addEventListener("DOMContentLoaded", function() {
+  commonInit();
+});
+
+
+function commonInit() {
+  var touchstart = "ontouchstart" in window;
+  var userAgent = navigator.userAgent.toLowerCase();
+  var checkitem = [];
+  if (touchstart) {
+    browserAdd("touchmode");
+  }
+  if (userAgent.indexOf('samsung') > -1) {
+    browserAdd("samsung");
+  }
+
+  if (navigator.platform.indexOf('Win') > -1 || navigator.platform.indexOf('win') > -1) {
+    browserAdd("window");
+  }
+
+  if (userAgent.match(/iPad/i) || userAgent.match(/iPhone/i)) {
+    // iPad or iPhone
+    browserAdd("ios");
+  }
+
+
+  function browserAdd(opt) {
+    document.querySelector("html").classList.add(opt);
+  }
+}
